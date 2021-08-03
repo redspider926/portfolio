5419574ec3808433cd34bf4f8b7408bd08f0bfcb
redspider926 <stonetiger1225@gmail.com>
2021-08-03 17:01:33 +0800

    added title and navbar to layout

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,7 +1,30 @@
 import React from 'react'
+import HomeIcon from '../assets/images/icons/home.svg'
 
 const Layout: React.FC = ({ children }) => {
-    return <div>{children}</div>
+    return <div className="layout">
+        <div className="layout-header">
+            <div className="layout-header__logo"></div>
+            <div className="layout-header__nav">
+                <div className="layout-header__nav-item">
+                    HOME
+                </div>
+                <div className="layout-header__nav-item">
+                    WORKS
+                </div>
+                <div className="layout-header__nav-item">
+                    SKILLS
+                </div>
+                <div className="layout-header__nav-item">
+                    ABOUT
+                </div>
+                <div className="layout-header__nav-item">
+                    CONTACT
+                </div>
+            </div>
+        </div>
+        {children}
+    </div>
 }
 
 export default Layout
