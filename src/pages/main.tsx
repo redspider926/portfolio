cd38a466613fa1d4878e6f1c33a15bb9d3e62cfe
redspider926 <stonetiger1225@gmail.com>
2021-08-03 10:24:14 +0800

    init commit

---
+++ src/pages/main.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Layout } from '../components'
+
+const PageMain: React.FC = () => {
+    return <Layout>
+        <div className="page-main">
+            <div className="page-main__section-1"></div>
+
+            <div className="page-main__section-2"></div>
+
+            <div className="page-main__section-3"></div>
+
+            <div className="page-main__section-4"></div>
+        </div>
+    </Layout>
+}
+
+export default PageMain
