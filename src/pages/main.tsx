f78eaf4d8096ec21908b5ed98028b54d53923356
redspider926 <stonetiger1225@gmail.com>
2021-08-03 14:54:53 +0800

    made file structure

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+// import Particles from 'react-tsparticles'
 import { Layout } from '../components'
+
 
 const PageMain: React.FC = () => {
     return <Layout>
         <div className="page-main">
-            <div className="page-main__section-1"></div>
+            <div className="page-main__section-1">
+                <div className="page-main__section-1__container">
+                    <h1>Successful Development</h1>
+                    <h2>Hi. I’m Hu Piao</h2>
+                </div>
+            </div>
 
             <div className="page-main__section-2"></div>
 
