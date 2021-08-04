6f97705862e1339487c23abcd90856fcc99979c2
redspider926 <stonetiger1225@gmail.com>
2021-08-04 21:28:06 +0800

    made about me part

--- src/pages/main.tsx
+++ src/pages/main.tsx
@@ -1,6 +1,9 @@
 import React from 'react'
 // import Particles from 'react-tsparticles'
 import { Layout, Skill, Work } from '../components'
+import AvatarSource from '../assets/images/avatar.jpg'
+import LinkedInLogo from '../assets/images/icons/linkedin-logo.svg'
+import GithubLogo from '../assets/images/icons/github-logo.svg'
 
 
 const PageMain: React.FC = () => {
@@ -38,6 +41,28 @@ const PageMain: React.FC = () => {
 
             <div className="page-main__section-4">
                 <h2 className="page-main__section-4__title section-title">ABOUT ME</h2>
+
+                <div className="page-main__section-4__panel">
+                    <div className="page-main__section-4__panel-left">
+                        <img src={AvatarSource} className="page-main__section-4__panel-left__avatar" alt="avatar" />
+                    </div>
+
+                    <div className="page-main__section-4__panel-right">
+                        I can help You with any stage of App development. It could include initial idea brainstorming, designing, app development and maintenance of an app on App Store. Sometimes I can’t fulfil your wishes alone. In this case, I’m also a capable project manager. I have an extensive network of other freelance developers who can help with more demanding tasks.
+                        I love new technologies and am always looking for interesting and challenging projects to work on. Currently I’m digging into Flutter for cross-platform development and SwiftUI for future native iOS projects.
+                        Don’t hesitate to contact me if you’re looking for an iOS developer. I can consult you on app development costs, technologies involved and give you a ballpark estimate on your idea rather quick. Later on I’ll be happy to assist you with production of the app.
+
+                        <div className="page-main__section-4__panel-right__social">
+                            <img src={LinkedInLogo} alt="linkedin-logo" />
+                            <a href="https://www.linkedin.com/in/piao-hu">https://www.linkedin.com/in/piao-hu</a>
+                        </div>
+
+                        <div className="page-main__section-4__panel-right__social">
+                            <img src={GithubLogo} alt="linkedin-logo" />
+                            <a href="https://github.com/redspider926">https://github.com/redspider926</a>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     </Layout>
